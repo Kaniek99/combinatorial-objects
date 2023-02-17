987891603532242ee4bbb85d83eeb22647fa034b
Kaniek <kaniek99@gmail.com>
2023-02-17 13:23:32 +0100

    before refatorization

--- internal/gui/windows/windows.go
+++ internal/gui/windows/windows.go
@@ -1,31 +1,41 @@
 package windows
 
-import (
-	"image/color"
+import "fyne.io/fyne/v2"
 
-	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/canvas"
-	"fyne.io/fyne/v2/container"
-)
+// "image/color"
+
+// "fyne.io/fyne/v2"
+// "fyne.io/fyne/v2/app"
+// "fyne.io/fyne/v2/canvas"
+// "fyne.io/fyne/v2/container"
 
 type WindowConfig struct {
-	// Width float32
-	// Hight float32
+	Width float32
+	Hight float32
 
-	// Application *fyne.App
+	Application fyne.App
 	// Window      *fyne.Window
 }
 
-func (window WindowConfig) Run() {
-	myApp := app.New()
-	mainWindow := myApp.NewWindow("Hello World")
-
-	top := canvas.NewText("top bar", color.Black)
-	left := canvas.NewText("left", color.Black)
-	middle := canvas.NewText("content", color.Black)
-	content := container.NewBorder(top, nil, left, nil, middle)
-	mainWindow.Resize(fyne.NewSize(1280, 720))
-	mainWindow.SetContent(content)
-	mainWindow.ShowAndRun()
+func (wc *WindowConfig) Run(name string, content fyne.CanvasObject) {
+	window := wc.Application.NewWindow(name)
+	window.Resize(fyne.NewSize(wc.Width, wc.Hight))
+	window.SetContent(content)
+	window.ShowAndRun()
 }
+
+// func (window WindowConfig) Run() {
+// 	app := app.New()
+
+// 	main := WindowConfig{1280, 720, &app}
+// 	mainWindow := app.NewWindow("Hello World")
+
+// 	top := canvas.NewText("Generating Combinatorial Objects", color.Black)
+//  left := canvas.NewText("left", color.Black)
+// 	buttons := container.NewWithoutLayout()
+// 	middle := canvas.NewText("content", color.Black)
+// 	content := container.NewBorder(top, nil, buttons, nil, middle)
+// 	mainWindow.Resize(fyne.NewSize(main.Width, main.Hight))
+// 	mainWindow.SetContent(content)
+// 	mainWindow.ShowAndRun()
+// }
--- main.go
+++ main.go
@@ -1,10 +1,25 @@
 package main
 
 import (
+	"image/color"
 	"kaniek99/combinatorial-objects/internal/gui/windows"
+
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
 )
 
 func main() {
-	mainWindow := windows.WindowConfig{}
-	mainWindow.Run()
+	app := app.New()
+	main := windows.WindowConfig{1280, 720, app}
+
+	top := canvas.NewText("Generating Combinatorial Objects", color.Black)
+	buttons := container.NewWithoutLayout()
+	middle := canvas.NewText("content", color.Black)
+	content := container.NewBorder(top, nil, buttons, nil, middle)
+
+	main.Run("Generating Combinatorial Objects", content)
+
+	// mainWindow := windows.WindowConfig{}
+	// mainWindow.Run()
 }

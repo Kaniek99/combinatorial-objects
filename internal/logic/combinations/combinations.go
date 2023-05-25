612e2de8cfefae37762fe2aee4e5fa0605246c68
Kaniek <kaniek99@gmail.com>
2023-05-25 23:40:28 +0200

    add implementation of algorithm to generate all combinations of n-set, output in terminal not in new window

--- internal/logic/combinations/combinations.go
+++ internal/logic/combinations/combinations.go
@@ -2,8 +2,9 @@ package combinations
 
 import (
 	"fmt"
-	"log"
 	"math"
+	"strconv"
+	"time"
 )
 
 type Combination map[string]bool
@@ -14,60 +15,74 @@ type Set struct {
 	Subsets     []Combination
 }
 
+func timer(name string) func() {
+	start := time.Now()
+	return func() {
+		fmt.Printf("%s took %v\n", name, time.Since(start))
+	}
+}
+
+func GenerateSet(input string) (Set, error) {
+	defer timer("GenerateSet")()
+	cardinality, err := strconv.Atoi(input)
+	if err != nil || cardinality < 0 {
+		return Set{}, fmt.Errorf("Insert integer greater than or equal to zero.")
+	}
+	var set []string
+	for i := cardinality - 1; i >= 0; i-- {
+		set = append(set, fmt.Sprint("x_", i))
+	}
+	return Set{Cardinality: cardinality, Set: set}, nil
+}
+
 func (set *Set) GenerateSubsets() {
+	defer timer("GenerateSubsets")()
 	for i := 0; i < int(math.Pow(2.0, float64(set.Cardinality))); i++ {
 		combination := make(Combination)
 
-		for j := 0; j < set.Cardinality; j++ { // what with empty set
+		for j := 0; j < set.Cardinality; j++ {
 			elemName := fmt.Sprint("x_", j)
 			combination[elemName] = false
 		} // now every combination is equal to empty set
 
-		test := fmt.Sprintf("%b", i)
-		fmt.Println(test)
-		for j, elem := range test {
+		binaryRepresentation := fmt.Sprintf("%b", i)
+		for j, elem := range binaryRepresentation {
 			if elem == '1' {
-				combination[fmt.Sprint("x_", len(test)-j-1)] = true
+				combination[fmt.Sprint("x_", len(binaryRepresentation)-j-1)] = true
 			} else {
-				combination[fmt.Sprint("x_", len(test)-j-1)] = false
+				combination[fmt.Sprint("x_", len(binaryRepresentation)-j-1)] = false
 			}
 		}
 		set.Subsets = append(set.Subsets, combination)
 	}
 }
 
-func (set *Set) PrintSet() {
-	// for _, elem := range set.Set {
-
-	// }
-	fmt.Println(set.Set)
+func (set *Set) GetSet() string {
+	output := "{"
+	for i, elem := range set.Set {
+		output += elem
+		if i < set.Cardinality-1 {
+			output += ", "
+		}
+	}
+	output += "}"
+	return output
 }
 
-func (set *Set) GetCombinations() [][]string {
-	combinations := [][]string{}
-	for _, combination := range set.Subsets {
-		subset := []string{}
-		for elem, presence := range combination {
-			if presence == true {
-				subset = append(subset, elem)
+func (set *Set) GetCombinations() string {
+	combinations := ""
+	for i, subset := range set.Subsets {
+		combination := []string{}
+		for elem, presence := range subset {
+			if presence {
+				combination = append(combination, elem)
 			}
 		}
-		combinations = append(combinations, subset)
+		CombinationAsSet := Set{Cardinality: len(combination), Set: combination}
+		combinations += CombinationAsSet.GetSet()
+		if i < len(set.Subsets)-1 {
+			combinations += ", "
+		}
 	}
 	return combinations
-	// fmt.Println(set.Subsets)
-}
-
-func GenerateSet(cardinality int) Set {
-	// var cardinality int
-	// fmt.Scan(&cardinality)
-	// if cardinality < 0 {
-	// retry, cardinality need to be uint
-	// }
-	log.Println("cardinality is: ", cardinality)
-	var set []string
-	for i := cardinality - 1; i >= 0; i-- {
-		set = append(set, fmt.Sprint("x_", i))
-	}
-	return Set{Cardinality: cardinality, Set: set}
 }
